6747eda2d28a427b00e1129a332d4461876e137d
Rafael G. Mantovani <rgmantovani@gmail.com>
2017-01-05 16:51:14 -0200

    controling the number of initial points for SMBO

--- R/getSMBOControl.R
+++ R/getSMBOControl.R
@@ -1,16 +1,16 @@
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
 
-getSMBOControl = function(par.set, budget) {
+getSMBOControl = function(par.set, budget, n.init.points = 10) {
 
   ctrl = makeMBOControl(
     impute.y.fun = function(x, y, opt.path) { return(0) }
   )
 
-  ctrl = setMBOControlTermination(ctrl, iters = budget)
+  ctrl = setMBOControlTermination(ctrl, iters = budget - n.init.points)
   ctrl = setMBOControlInfill(ctrl, crit = "ei", opt = "focussearch", opt.restarts = 2L,
     opt.focussearch.points = 500L)
-  design = generateRandomDesign(n = 10, par.set = par.set)
+  design = generateRandomDesign(n = n.init.points, par.set = par.set)
 
   surrogate.model = makeImputeWrapper(
     learner = makeLearner("regr.randomForest", 
