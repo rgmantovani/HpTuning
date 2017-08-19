9dc64ea98b985309b800436dedf30c7c708d29ec
Rafael G. Mantovani <rgmantovani@gmail.com>
2017-08-19 16:51:15 -0300

    updating usp bash file

--- runUsp.sh
+++ runUsp.sh
@@ -2,22 +2,29 @@
 
 DIR='data';
 EXT='\.arff';
-DATASETS=`ls ./$DIR/*$EXT | sed -e "s/\.\/$DIR\///g" | sed -e "s/$EXT//g"`
+DATASETS=(`ls ./$DIR/*$EXT | sed -e "s/\.\/$DIR\///g" | sed -e "s/$EXT//g"`)
 
-for data in $DATASETS
+FROM=0
+TO=220
+#TO=${#DATASETS[@]} # all elements
+REPS=10
+
+for id in $(seq $FROM $TO);
 do
-  # echo $data
-  for algo in "classif.J48" # "classif.rpart" "classif.svm"
+  for algo in "classif.svm"
   do
-    for tuning in "defaults" "random" "mbo" "irace"
+    for tuning in "defaults" #"random" "mbo" "irace"
     do
-      for rep in $(seq 1 30);
+      for rep in $(seq 1 $REPS);
       do
-        R CMD BATCH --no-save --no-restore '--args' --datafile="$data" --algo="$algo " --tuning="$tuning" \
-           --epoch="$rep" mainHP.R out_"$data"_"$algo"_"$tuning"_rep_"$rep".log &
-            PIDS[$rep]=$!
+        echo ${DATASETS[$id]} $algo $tuning $rep out_"${DATASETS[$id]}"_"$algo"_"$tuning"_rep_"$rep".log
+        
+	R CMD BATCH  --no-save --no-restore '--args' --datafile="${DATASETS[$id]}" --algo="$algo " --tuning="$tuning" \
+           --epoch="$rep" mainHP.R out_"${DATASETS[$id]}"_"$algo"_"$tuning"_rep_"$rep".log &
+       
+	#PIDS[$rep]=$!
       done
-      for k in $(seq 1 30);
+      for k in $(seq 1 $REPS);
       do
         wait ${PIDS[$k]}
       done;
