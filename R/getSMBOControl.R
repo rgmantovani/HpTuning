2fedae40da6e9e1492a778ad4642df349e4d3f04
Rafael G. Mantovani <rgmantovani@gmail.com>
2018-04-18 00:57:02 -0300

    fixing mlrMBO inner commands

--- R/getSMBOControl.R
+++ R/getSMBOControl.R
@@ -3,14 +3,14 @@
 
 getSMBOControl = function(par.set, budget, n.init.points = 10) {
 
-  ctrl = makeMBOControl(
+  ctrl = mlrMBO::makeMBOControl(
     impute.y.fun = function(x, y, opt.path) { return(0) }
   )
 
-  ctrl = setMBOControlTermination(ctrl, iters = budget - n.init.points)
-  ctrl = setMBOControlInfill(ctrl, crit = "ei", opt = "focussearch", opt.restarts = 2L,
-    opt.focussearch.points = 500L)
-  design = generateRandomDesign(n = n.init.points, par.set = par.set)
+  ctrl = mlrMBO::setMBOControlTermination(control = ctrl, iters = budget - n.init.points)
+  ctrl = mlrMBO::setMBOControlInfill(ctrl, crit = mlrMBO::crit.ei, opt = "focussearch",
+   opt.restarts = 2L, opt.focussearch.points = 500L)
+  design = ParamHelpers::generateRandomDesign(n = n.init.points, par.set = par.set)
 
   surrogate.model = makeImputeWrapper(
     learner = makeLearner("regr.randomForest", 
@@ -21,6 +21,7 @@ getSMBOControl = function(par.set, budget, n.init.points = 10) {
   
   ctrl = mlr:::makeTuneControlMBO(learner = surrogate.model, mbo.control = ctrl, 
     mbo.design = design)
+ 
   return(ctrl)
 }
 
