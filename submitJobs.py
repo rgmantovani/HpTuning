95a72366bdbeca8f8e2255e4f71e2c72d4818eb9
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2021-05-26 15:46:14 -0300

    fixing memory errors

--- submitJobs.py
+++ submitJobs.py
@@ -3,34 +3,36 @@ from multiprocessing import Pool
 from os import listdir
 from os.path import isfile, join
 
+# execute R job
 def myjob(op):
-    #command = "Rscript mainHP.R --datafile="+op[0]+" --algo="+op[1]+" --tuning="+op[2]+" --epoch="+op[3]
     command = "R CMD BATCH --no-save --no-restore \'--args\' --datafile="+op[0]+" --algo="+op[1]+" --tuning="+op[2]+" --epoch="+op[3]+" mainHP.R job_"+op[0]+"_"+op[1]+"_"+op[2]+"_"+op[3]+".log"
-    #print(command)
-    # execute the command
     os.system(command.format(*op))
 
+# main function
 def main():
+
+    #list all dataset files
     mypath = "./data/"
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+    # remove .arff extension
     datasets = []
     for string in onlyfiles:
         new_string = string.replace(".arff", "")
         datasets.append(new_string)
 
+    # define other job parameters
     algo  = ["classif.svm"]  # Algorithms to be tuned
     tuner = ["mbo"]          # HP tuning technique
 
     # seeds from 1 to 10
     seed = [str(i) for i in range(1,11)]
 
-    # script's parameters (options)
+    # combine all the script's parameters (options)
     options = itertools.product(datasets, algo, tuner, seed)
-    #for opt in options:
-    #    print(opt)
 
-    with Pool(processes=20) as pool:
+    # start a pool of workers (threads) and schedule them
+    with Pool(processes=20, maxtasksperchild=1)) as pool:
         # map blocks until the result is ready
         results = pool.imap(myjob, options, chunksize=1)
         #TODO: add timeout (100 hs)
